6a80b672df5bdc7d16da12a68a5bd5b36d21c45f
MOBIUS <yuchenliu0710@outlook.com>
2025-01-10 11:51:39 -0800

    add player movement and link things up in Unity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,9 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private float _speed = 7.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
@@ -13,16 +14,44 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
+        // Update UI information
         
     }
 
     private void Update()
     {
+        // Player Movement
+        Vector3 direction = new Vector3();
         
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector3.right;
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector3.up;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector3.down;
+        direction = direction.normalized;
+        
+        _playerTransform.Translate(direction * _speed * Time.deltaTime);
+        
+        // Player Plant Action
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            // Plant
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            _numSeeds--;
+                
+            // Update UI
+            
+        }
     }
 }
